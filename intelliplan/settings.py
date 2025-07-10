34e1645e2937168b91ce0f48ff99e369b7d7afb8
Suyog Kumar Pokhrel <suyogpokhrel13@gmail.com>
2025-07-10 18:37:14 +0545

    Update ALLOWED_HOSTS in settings.py

--- intelliplan/settings.py
+++ intelliplan/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-temporary-key-for-dev
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
-
+ALLOWED_HOSTS = ['https://intelliplan1.onrender.com/']
 
 # Application definition
 
